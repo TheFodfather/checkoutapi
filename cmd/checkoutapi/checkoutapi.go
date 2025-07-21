860844e36f5aa413d61ed92307d6a89118b2fa56
TheFodfather <spencerst98@googlemail.com>
2025-07-21 19:13:22 +0100

    feat: add application entrypoint and startup logic

--- cmd/checkoutapi/checkoutapi.go
+++ cmd/checkoutapi/checkoutapi.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	pricingSvc "github.com/TheFodfather/checkoutapi/pricing/service"
+
+	"github.com/TheFodfather/checkoutapi/checkout/handler"
+	"github.com/TheFodfather/checkoutapi/checkout/repository"
+)
+
+func main() {
+	pricer, err := pricingSvc.New("./cmd/configs/pricing.json")
+	if err != nil {
+		log.Fatalf("❌ Could not start pricing service - err=%q", err)
+	}
+
+	repo := repository.NewInMemoryRepository()
+	httpHandler := handler.New(repo, pricer)
+
+	mux := http.NewServeMux()
+	httpHandler.RegisterRoutes(mux)
+
+	port := "8080"
+	log.Printf("🚀 Starting server on http://localhost:%s", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatalf("❌ Could not start server - err=%q", err)
+	}
 }
